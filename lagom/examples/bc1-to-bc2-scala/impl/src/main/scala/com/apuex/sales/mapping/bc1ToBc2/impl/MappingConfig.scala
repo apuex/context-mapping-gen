3c72a9d605589853db7c3f93c75b232eb0b819ef
Wangxy <xtwxy@hotmail.com>
2019-05-14 23:11:28 +0800

    add json serializer.

--- lagom/examples/bc1-to-bc2-scala/impl/src/main/scala/com/apuex/sales/mapping/bc1ToBc2/impl/MappingConfig.scala
+++ lagom/examples/bc1-to-bc2-scala/impl/src/main/scala/com/apuex/sales/mapping/bc1ToBc2/impl/MappingConfig.scala
@@ -6,7 +6,9 @@ import java.util.concurrent.TimeUnit
 import akka.stream.scaladsl.Source
 import com.github.apuex.events.play._
 import com.github.apuex.protobuf.serializer.AnyPackagerBuilder
+import com.google.protobuf.Message
 import com.google.protobuf.util.JsonFormat
+import play.api.libs.json._
 
 import scala.concurrent.duration.Duration
 
@@ -42,4 +44,19 @@ class MappingConfig {
     parser.merge(json, builder)
     builder.build
   }
+
+  class MessageFormat[T<: Message](val clazz: Class[T]) extends OFormat[T] {
+    val builderMethod = clazz.getDeclaredMethod("newBuilder")
+    override def reads(json: JsValue): JsResult[T] = {
+      val builder = builderMethod.invoke(null).asInstanceOf[Message.Builder]
+      parser.merge(json.toString(), builder)
+      JsSuccess(builder.build().asInstanceOf[T])
+    }
+
+    override def writes(o: T): JsObject = Json.parse(
+      printer.print(o)
+    ).validate[JsObject].get
+  }
+
+  def jsonFormat[T<: Message](clazz: Class[T]): OFormat[T] = new MessageFormat(clazz)
 }
--- lagom/examples/bc1-to-bc2-scala/impl/src/main/scala/com/apuex/sales/mapping/bc1ToBc2/impl/OrderInventoryMappingLoader.scala
+++ lagom/examples/bc1-to-bc2-scala/impl/src/main/scala/com/apuex/sales/mapping/bc1ToBc2/impl/OrderInventoryMappingLoader.scala
@@ -4,9 +4,18 @@ import akka.actor.Props
 import com.apuex.sales.mapping.bc1ToBc2._
 import com.lightbend.lagom.scaladsl.client._
 import com.lightbend.lagom.scaladsl.devmode._
+import com.lightbend.lagom.scaladsl.playjson.{JsonSerializer, JsonSerializerRegistry}
 import com.lightbend.lagom.scaladsl.server._
 import com.softwaremill.macwire._
 import play.api.libs.ws.ahc._
+import com.lightbend.lagom.scaladsl.api.ServiceLocator
+import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
+import com.lightbend.lagom.scaladsl.server._
+import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
+import play.api.libs.ws.ahc.AhcWSComponents
+import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
+import com.softwaremill.macwire._
+import scala.collection.immutable.Seq
 
 class OrderInventoryMappingLoader extends LagomApplicationLoader {
 
@@ -16,21 +25,35 @@ class OrderInventoryMappingLoader extends LagomApplicationLoader {
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
     new OrderInventoryMappingApplication(context) with LagomDevModeComponents
 
+  override def describeService = Some(readDescriptor[OrderInventoryService])
 }
 
 abstract class OrderInventoryMappingApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
     with AhcWSComponents {
 
+  lazy val mappingConfig = new MappingConfig()
   // Bind the service that this server provides
   override lazy val lagomServer: LagomServer = serverFor[OrderInventoryService](wire[OrderInventoryServiceImpl])
+  // Register the JSON serializer registry
+  override lazy val optionalJsonSerializerRegistry = Some(new JsonSerializerRegistry {
+    import mappingConfig._
+    override def serializers: Seq[JsonSerializer[_]] = Seq(
+      JsonSerializer[ReduceStorageCmd](jsonFormat(classOf[ReduceStorageCmd])),
+      JsonSerializer[RetrieveOrderCmd](jsonFormat(classOf[RetrieveOrderCmd])),
+      JsonSerializer[PayOrderEvt](jsonFormat(classOf[PayOrderEvt])),
+      JsonSerializer[OrderItemVo](jsonFormat(classOf[OrderItemVo])),
+      JsonSerializer[OrderVo](jsonFormat(classOf[OrderVo])),
+      JsonSerializer[RetrieveProductCmd](jsonFormat(classOf[RetrieveProductCmd])),
+      JsonSerializer[ProductVo](jsonFormat(classOf[ProductVo]))
+    )
+  })
 
   // Bind the service clients
   lazy val orderService: OrderService = serviceClient.implement[OrderService]
   lazy val productService: ProductService = serviceClient.implement[ProductService]
   lazy val inventoryService: InventoryService = serviceClient.implement[InventoryService]
 
-  val mappingConfig = new MappingConfig()
   val mapping = actorSystem.actorOf(
     Props(new OrderInventoryMapping(
       mappingConfig,
