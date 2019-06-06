04993974b7ae79644f7a8b1bd87dbe54719f8ce9
Wangxy <xtwxy@hotmail.com>
2019-06-07 00:35:27 +0800

    implement insert tests.

--- table/src/test/scala/com/github/apuex/ctxmapgen/table/TableMappingGeneratorSpec.scala
+++ table/src/test/scala/com/github/apuex/ctxmapgen/table/TableMappingGeneratorSpec.scala
@@ -9,15 +9,240 @@ class TableMappingGeneratorSpec extends FlatSpec with Matchers {
   import m._
 
   "A TableMappingGenerator" should "generate table mapping from rowid" in {
+    val table =
+      <src-table name="src_table_1">
+        <!--
+          key columns, or rowid
+        -->
+        <key>
+          <column name="col_1"/>
+          <column name="col_2"/>
+        </key>
+        <dest-table name="dest_table_1">
+          <column no="1" name="col_1" from-column="col_1"/>
+          <column no="2" name="col_2" from-column="col_2"/>
+          <column no="3" name="col_3" from-column="col_3"/>
+          <column no="4" name="col_4" from-column="col_4"/>
+          <key>
+            <column name="col_1"/>
+            <column name="col_2"/>
+          </key>
+        </dest-table>
+        <!--
+          affected views by source table change.
+        -->
+        <view name="src_view_1">
+          <key>
+            <column name="col_1"/>
+            <column name="col_2"/>
+          </key>
+          <dest-table name="dest_table_2">
+            <column no="1" name="col_1" from-column="col_1"/>
+            <column no="2" name="col_2" from-column="col_2"/>
+            <column no="3" name="col_3" from-column="col_3"/>
+            <column no="4" name="col_4" from-column="col_4"/>
+            <key>
+              <column name="col_1"/>
+              <column name="col_2"/>
+            </key>
+          </dest-table>
+        </view>
+        <view name="src_view_2">
+          <key>
+            <column name="col_1"/>
+          </key>
+          <dest-table name="dest_table_5">
+            <column no="1" name="col_1" from-column="col_1"/>
+            <column no="2" name="col_2" from-column="col_2"/>
+            <column no="3" name="col_3" from-column="col_3"/>
+            <key>
+              <column name="col_1"/>
+            </key>
+          </dest-table>
+        </view>
+      </src-table>
+
     fail("test not implemented.")
+    val tableMapping = generateTableMapping(table)
+    tableMapping._1 should be("src_table_1")
+    tableMapping._2 should be(
+      s"""
+         |class SrcTable1Mapping (src: SrcService, dest: DestService) extends TableMapping {
+         |
+         |  override def create(tableName: String, rowid: String): Unit = {
+         |    src.retrieveSrcTable1ByRowid().invoke(RetrieveByRowidCmd(evt.rowid))
+         |      .map(t => {
+         |        dest.createDestTable1().invoke(CreateDestTable1Cmd(t.col1, t.col2, t.col3, t.col4))
+         |        src.retrieveSrcView1().invoke(RetrieveSrcView1Cmd(t.col1, t.col2))
+         |          .map(v => {
+         |            dest.createDestTable2().invoke(CreateDestTable2Cmd(v.col1, v.col2, v.col3, v.col4))
+         |          })
+         |        src.retrieveSrcView2().invoke(RetrieveSrcView2Cmd(t.col1))
+         |          .map(v => {
+         |            dest.createDestTable5().invoke(CreateDestTable5Cmd(v.col1, v.col2, v.col3))
+         |          })
+         |      })
+         |  }
+         |
+         |  override def update(tableName: String, rowid: String): Unit = {
+         |    src.retrieveSrcTable1ByRowid().invoke(RetrieveByRowidCmd(evt.rowid))
+         |      .map(t => {
+         |        dest.updateDestTable1().invoke(UpdateDestTable1Cmd(t.col1, t.col2, t.col3, t.col4))
+         |        src.retrieveSrcView1().invoke(RetrieveSrcView1Cmd(t.col1, t.col2))
+         |          .map(v => {
+         |            dest.updateDestTable2().invoke(UpdateDestTable2Cmd(v.col1, v.col2, v.col3, v.col4))
+         |          })
+         |        src.retrieveSrcView2().invoke(RetrieveSrcView2Cmd(t.col1))
+         |          .map(v => {
+         |            dest.updateDestTable5().invoke(UpdateDestTable5Cmd(v.col1, v.col2, v.col3))
+         |          })
+         |      })
+         |  }
+         |
+         |  override def delete(tableName: String, rowid: String): Unit = {
+         |
+         |  }
+         |}
+       """.stripMargin.trim)
   }
 
   it should "generate insert dest-table from rowid" in {
-    fail("test not implemented.")
+    val table =
+      <src-table name="src_table_1">
+        <!--
+          key columns, or rowid
+        -->
+        <key>
+          <column name="col_1"/>
+          <column name="col_2"/>
+        </key>
+        <dest-table name="dest_table_1">
+          <column no="1" name="col_1" from-column="col_1"/>
+          <column no="2" name="col_2" from-column="col_2"/>
+          <column no="3" name="col_3" from-column="col_3"/>
+          <column no="4" name="col_4" from-column="col_4"/>
+          <key>
+            <column name="col_1"/>
+            <column name="col_2"/>
+          </key>
+        </dest-table>
+        <!--
+          affected views by source table change.
+        -->
+        <view name="src_view_1">
+          <key>
+            <column name="col_1"/>
+            <column name="col_2"/>
+          </key>
+          <dest-table name="dest_table_2">
+            <column no="1" name="col_1" from-column="col_1"/>
+            <column no="2" name="col_2" from-column="col_2"/>
+            <column no="3" name="col_3" from-column="col_3"/>
+            <column no="4" name="col_4" from-column="col_4"/>
+            <key>
+              <column name="col_1"/>
+              <column name="col_2"/>
+            </key>
+          </dest-table>
+        </view>
+        <view name="src_view_2">
+          <key>
+            <column name="col_1"/>
+          </key>
+          <dest-table name="dest_table_5">
+            <column no="1" name="col_1" from-column="col_1"/>
+            <column no="2" name="col_2" from-column="col_2"/>
+            <column no="3" name="col_3" from-column="col_3"/>
+            <key>
+              <column name="col_1"/>
+            </key>
+          </dest-table>
+        </view>
+      </src-table>
+
+    insertFromRowId(table) should be(
+      s"""
+         |src.retrieveSrcTable1ByRowid().invoke(RetrieveByRowidCmd(evt.rowid))
+         |  .map(t => {
+         |    dest.createDestTable1().invoke(CreateDestTable1Cmd(t.col1, t.col2, t.col3, t.col4))
+         |    src.retrieveSrcView1().invoke(RetrieveSrcView1Cmd(t.col1, t.col2))
+         |      .map(v => {
+         |        dest.createDestTable2().invoke(CreateDestTable2Cmd(v.col1, v.col2, v.col3, v.col4))
+         |      })
+         |    src.retrieveSrcView2().invoke(RetrieveSrcView2Cmd(t.col1))
+         |      .map(v => {
+         |        dest.createDestTable5().invoke(CreateDestTable5Cmd(v.col1, v.col2, v.col3))
+         |      })
+         |  })
+       """.stripMargin.trim)
   }
 
   it should "generate insert dest-table from src-table" in {
-    fail("test not implemented.")
+    val table =
+      <src-table name="src_table_1">
+        <!--
+          key columns, or rowid
+        -->
+        <key>
+          <column name="col_1"/>
+          <column name="col_2"/>
+        </key>
+        <dest-table name="dest_table_1">
+          <column no="1" name="col_1" from-column="col_1"/>
+          <column no="2" name="col_2" from-column="col_2"/>
+          <column no="3" name="col_3" from-column="col_3"/>
+          <column no="4" name="col_4" from-column="col_4"/>
+          <key>
+            <column name="col_1"/>
+            <column name="col_2"/>
+          </key>
+        </dest-table>
+        <!--
+          affected views by source table change.
+        -->
+        <view name="src_view_1">
+          <key>
+            <column name="col_1"/>
+            <column name="col_2"/>
+          </key>
+          <dest-table name="dest_table_2">
+            <column no="1" name="col_1" from-column="col_1"/>
+            <column no="2" name="col_2" from-column="col_2"/>
+            <column no="3" name="col_3" from-column="col_3"/>
+            <column no="4" name="col_4" from-column="col_4"/>
+            <key>
+              <column name="col_1"/>
+              <column name="col_2"/>
+            </key>
+          </dest-table>
+        </view>
+        <view name="src_view_2">
+          <key>
+            <column name="col_1"/>
+          </key>
+          <dest-table name="dest_table_5">
+            <column no="1" name="col_1" from-column="col_1"/>
+            <column no="2" name="col_2" from-column="col_2"/>
+            <column no="3" name="col_3" from-column="col_3"/>
+            <key>
+              <column name="col_1"/>
+            </key>
+          </dest-table>
+        </view>
+      </src-table>
+
+    insertFromTableMapping(table, "t") should be(
+      s"""
+         |dest.createDestTable1().invoke(CreateDestTable1Cmd(t.col1, t.col2, t.col3, t.col4))
+         |src.retrieveSrcView1().invoke(RetrieveSrcView1Cmd(t.col1, t.col2))
+         |  .map(v => {
+         |    dest.createDestTable2().invoke(CreateDestTable2Cmd(v.col1, v.col2, v.col3, v.col4))
+         |  })
+         |src.retrieveSrcView2().invoke(RetrieveSrcView2Cmd(t.col1))
+         |  .map(v => {
+         |    dest.createDestTable5().invoke(CreateDestTable5Cmd(v.col1, v.col2, v.col3))
+         |  })
+       """.stripMargin.trim)
   }
 
   it should "generate insert dest-table from view" in {
