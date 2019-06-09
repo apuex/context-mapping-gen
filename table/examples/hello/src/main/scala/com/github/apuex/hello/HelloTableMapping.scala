05f27f71a2df4aa84bcfb755bac14cc3e2cce6ae
Wangxy <xtwxy@hotmail.com>
2019-06-09 11:45:24 +0800

    implement poc on delete by rowid.

--- table/examples/hello/src/main/scala/com/github/apuex/hello/HelloTableMapping.scala
+++ table/examples/hello/src/main/scala/com/github/apuex/hello/HelloTableMapping.scala
@@ -2,18 +2,26 @@ package com.github.apuex.hello
 
 import scala.concurrent.ExecutionContext
 
-class HelloTableMapping (src: SrcService, dest: DestService, implicit val ec: ExecutionContext) extends TableMapping {
+class HelloTableMapping (
+                          src: SrcService,
+                          dest: DestService,
+                          addDelete: (String, String, Any) => Unit,
+                          getDeletes: (String, String) => Seq[Any],
+                          implicit val ec: ExecutionContext) extends TableMapping {
 
   override def create(tableName: String, rowid: String): Unit = {
     src.retrieveSrcTable1ByRowid().invoke(RetrieveByRowidCmd(rowid))
       .map(t => {
+        addDelete(tableName, rowid, DeleteDestTable1Cmd(t.col1, t.col2))
         dest.createDestTable1().invoke(CreateDestTable1Cmd(t.col1, t.col2, t.col3, t.col4))
         src.retrieveSrcView1().invoke(RetrieveSrcView1Cmd(t.col1, t.col2))
           .map(v => {
+            addDelete(tableName, rowid, DeleteDestTable1Cmd(v.col1, v.col2))
             dest.createDestTable2().invoke(CreateDestTable2Cmd(v.col1, v.col2, v.col3, v.col4))
           })
         src.retrieveSrcView2().invoke(RetrieveSrcView2Cmd(t.col1))
           .map(v => {
+            addDelete(tableName, rowid, DeleteDestTable1Cmd(v.col1, v.col2))
             dest.createDestTable5().invoke(CreateDestTable5Cmd(v.col1, v.col2, v.col3))
           })
       })
@@ -35,6 +43,14 @@ class HelloTableMapping (src: SrcService, dest: DestService, implicit val ec: Ex
   }
 
   override def delete(tableName: String, rowid: String): Unit = {
-
+    getDeletes(tableName, rowid)
+      .foreach({
+        case x: DeleteDestTable1Cmd =>
+          dest.deleteDestTable1().invoke(x)
+        case x: DeleteDestTable2Cmd =>
+          dest.deleteDestTable2().invoke(x)
+        case x: DeleteDestTable5Cmd =>
+          dest.deleteDestTable5().invoke(x)
+      })
   }
 }
